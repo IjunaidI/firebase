7fef01259c6d7b9e9f1d96dfdc0c69086f91f7f6
IjunaidI <mjunaid7843@gmail.com>
2018-08-30 13:38:45 +0500

    updates

--- script.js
+++ script.js
@@ -1,85 +1,68 @@
 
+
 var database = firebase.database();
 var user = firebase.auth().currentUser;
-var name, email, photoUrl, uid, emailVerified,userName,placeCity,placeCountry;
+var name, email, photoUrl, uid, emailVerified, userName, placeCity, placeCountry;
 
 
-firebase.auth().onAuthStateChanged(function(user) {
-    if (user) {
-      document.querySelector(".invis").style.display = 'block';
-      document.querySelector(".invis").innerHTML = user.uid;
-      
-      var ref = firebase.database().ref(`users/${uid}`);
+firebase.auth().onAuthStateChanged(function (user) {
+  if (user) {
+    var uid = firebase.auth().currentUser.uid;
+    document.querySelector(".invis").style.display = 'block';
+    document.querySelector(".invis").innerHTML = user.uid;
+    var ref = firebase.database().ref(`users/${uid}`);
+    var uid = firebase.auth().currentUser.uid;
 
-      ref.on("value", function(snapshot) {
-         console.log(snapshot.val());
-      }, function (error) {
-         console.log("Error: " + error.code);
-      });
-    }
     
-     else {
-        document.querySelector(".invis").style.display = 'none';
-    }
+  }
+
+  else {
+    document.querySelector(".invis").style.display = 'none';
+  }
+});
+
+function signIn() {
+  var email = document.querySelector("#email").value;
+  var password = document.querySelector("#password").value;
+
+  firebase.auth().signInWithEmailAndPassword(email, password).catch(function (error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    window.alert(errorMessage);
   });
-
-function signIn(){ 
-    var email = document.querySelector("#email").value;
-    var password = document.querySelector("#password").value;
-    
-    firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
-        // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
-       window.alert(errorMessage);
-      });
 }
 
-function signUp(){ 
-    var email = document.querySelector("#email").value;
-    var password = document.querySelector("#password").value;
-    
-    firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
-        // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
-       window.alert(errorMessage);
+function signUp() {
+  var email = document.querySelector("#email").value;
+  var password = document.querySelector("#password").value;
+  let userCity = document.querySelector("#city").value;
 
+  firebase.auth().createUserWithEmailAndPassword(email, password)
+    .then(rs => {
+      console.log(rs.user.uid);
+      firebase.database().ref().child('accounts').child(rs.user.uid).set({
+        city: userCity
+      })
+    })
+    .catch(function (error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      window.alert(errorMessage)
 
-      });
-      document.querySelector(".info").style.display = 'flex';
-      document.querySelector(".log").style.display = 'none';
-    }
-  function info(){
-    var userName = document.querySelector("#username").value;
-    var placeCity = document.querySelector("#city").value;
-    var  placeCountry = document.querySelector("#country").value;
-if (user != null) {
-        name = userName;
-        photoUrl = "user.photoURL";
-        emailVerified = "none";
-        uid = user.uid;
-        city = placeCity;
-        country = placeCountry;  
-      }
-      if (user != null) {
-        writeUserData(uid, name, email, photoUrl,city,country);
-      }
-      function writeUserData(userId, name, email, imageUrl,city,country) {
-        firebase.database().ref('users/' + userId).set({
-          username: name,
-          email: email,
-          profile_picture : imageUrl,
-          place1  : city,
-          place2 : country
-        });
-      }
+    });
+  document.querySelector(".info").style.display = 'flex';
+  document.querySelector(".log").style.display = 'none';
 }
+//   function info(){
 
-function logOut(){
-firebase.auth().signOut().then(function() {
+// }
+
+function logOut() {
+  firebase.auth().signOut().then(function () {
     alert("signed out")
-  }).catch(function(error) {
+  }).catch(function (error) {
     window.alert(error)
   });
 }
